5a2037884098baf4cdbbf24d336798f5c0b5b4fe
cris-jac <cristian25co@hotmail.com>
2024-03-16 17:46:04 -0300

    Intial commit

---
+++ LaboratorioAws/Clases/ClubClass.cs
@@ -0,0 +1,12 @@
+﻿namespace LaboratorioAws.Clases
+{
+    public class ClubClass
+    {
+        public int id;
+        public string name;
+        public DateTime fundationFoundation_date;
+        public string manager;
+        public List<PlayerClass> players;
+        public StandingClass standing;
+    }
+}
---
+++ LaboratorioAws/Clases/StadiumClass.cs
@@ -0,0 +1,11 @@
+﻿namespace LaboratorioAws.Clases
+{
+    public class StadiumClass
+    {
+        public int id;
+        public string name;
+        public string location;
+        public int capacity;
+        public ClubClass owner;
+    }
+}
