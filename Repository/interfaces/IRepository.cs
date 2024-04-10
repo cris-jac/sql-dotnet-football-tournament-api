0aa03ca0351e42e334fa65cd8de7c544c6bf5dca
Fernando Bertone <fernando.bertone.98@gmail.com>
2024-04-10 18:32:45 -0300

    Separe el proyecto en net, model y repository. movi entitis al proyect model y cree algunso repository e interfaces en el porj repository

---
+++ Repository/interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.interfaces
+{
+    internal interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAll();
+        Task<T> GetId(int id);
+        Task Add(T entity);
+        void Delete(T entity);
+        void Edit(T entity);
+    }
+}
